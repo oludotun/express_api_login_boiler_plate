c57b6051e3425d76d99dc6dd6b548897b79b958b
Oludotun Ebiekuraju <dotun@intuitigo.com>
2020-11-29 11:25:03 +0100

    Added SQL comaand for creating db and users

---
+++ db/init.sql
@@ -0,0 +1,21 @@
+-- Delete existing database and create a new one
+DROP DATABASE IF EXISTS obha;
+CREATE DATABASE IF NOT EXISTS obha;
+USE obha;
+
+-- Drop all tables
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS sessions;
+
+-- Create the users table
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED AUTO_INCREMENT,
+    name VARCHAR(80) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    INDEX (email),
+    PRIMARY KEY (id)
+);
