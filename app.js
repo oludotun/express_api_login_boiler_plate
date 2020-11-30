be3ed80f33f6d66af4d5d747210f75a2f01053ce
Oludotun Ebiekuraju <dotun@intuitigo.com>
2020-11-30 21:51:17 +0100

    Added auth router to app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const pagesRouter = require('./routes/pages');
+const authRouter = require('./routes/auth');
 const app = express();
 const passport = require('passport');
 
+app.use(express.json());
+
+app.use('/', authRouter);
 app.use('/', pagesRouter);
 app.listen(process.env.PORT, () => {
     console.log(`Server started and running on port ${process.env.PORT}`);
