fae9cfca4aea8b779ec25c3d412800df7e3c84a0
Oludotun Ebiekuraju <dotun@intuitigo.com>
2020-12-02 23:52:54 +0100

    Refactored login route to return proper json when login failed

--- routes/auth.js
+++ routes/auth.js
@@ -5,19 +5,30 @@ const passport = require('passport');
 
 router.post('/signup', signUpValidator, signUp);
 router.post('/password/reset', resetPassword);
-router.post('/login', passport.authenticate('local'), (req, res, next) => {
-    req.session.save((err) => {
-        if (err) {
-            return next(err);
+router.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { 
+            return next(err); 
         }
-        return res.status(200).json({
-            status: "success",
-            message: "Logged in successfully.",
-            data: {
-                user: req.user
+        if (!user) { 
+            return res.status(401).json({
+                status: "error",
+                message: "Access denied! Invalid email or password."
+            }); 
+        }
+        req.logIn(user, function(err) {
+            if (err) { 
+                return next(err); 
             }
+            return res.status(200).json({
+                status: "success",
+                message: "Logged in successfully.",
+                data: {
+                    user: req.user
+                }
+            });
         });
-    });
+    })(req, res, next);
 });
 router.post('/logout', requireAuthentication, (req, res, next) => {
     req.logout();
