9ff0e8b38fea30503cc768adfb6919698d782691
Oludotun Ebiekuraju <dotun@intuitigo.com>
2020-12-02 23:39:38 +0100

    Added password reset route to auth router

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { signUp, signUpValidator } = require('../controllers/users');
+const { signUp, signUpValidator, resetPassword } = require('../controllers/users');
 const { requireAuthentication } = require('../middleware/auth');
 const passport = require('passport');
 
 router.post('/signup', signUpValidator, signUp);
+router.post('/password/reset', resetPassword);
 router.post('/login', passport.authenticate('local'), (req, res, next) => {
     req.session.save((err) => {
         if (err) {
