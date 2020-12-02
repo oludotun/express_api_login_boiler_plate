a935d9abe854128032fa0b538cf5f21f47cf6421
Oludotun Ebiekuraju <dotun@intuitigo.com>
2020-12-02 23:32:41 +0100

    Added a random string generator function for password reset

---
+++ services/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    randomString: (length, chars=null) => {
+        if(!chars) chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&+';
+        let result = '';
+        for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+        return result;
+    }
+}
