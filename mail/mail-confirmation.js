980225de331ae5d9db49a338c1c784e4d7a1474e
Oludotun Ebiekuraju <dotun@intuitigo.com>
2021-01-22 17:53:09 +0100

    Prevent dev server from sending emails
    Email is logged to the console on a development server

--- mail/mail-confirmation.js
+++ mail/mail-confirmation.js
@@ -4,17 +4,23 @@ const { appName } = require('../config/app');
 const MailConfig = require('../config/mail');
 
 const sendMailConfirmationMail = async (user, link, callback=null) => {
-    let info = await transporter.sendMail({
-        from: `"${MailConfig.from.name}" <${MailConfig.from.address}>`,
-        to: `"${user.name}" <${user.email}>`,
-        subject: `Please Verify Your ${appName} Email Address`,
-        text: MailConfirmationMail.text(user, link),
-        html: MailConfirmationMail.html(user, link),
-        headers: {
-            'Return-Path': MailConfig.bounce.email
-        }
-    });
-    if(callback) callback({result: `Mail sent successfully to ${user.email}`});
+    if(process.env.NODE_ENV !== 'development') {
+        let info = await transporter.sendMail({
+            from: `"${MailConfig.from.name}" <${MailConfig.from.address}>`,
+            to: `"${user.name}" <${user.email}>`,
+            subject: `Please Verify Your ${appName} Email Address`,
+            text: MailConfirmationMail.text(user, link),
+            html: MailConfirmationMail.html(user, link),
+            headers: {
+                'Return-Path': MailConfig.bounce.email
+            }
+        });
+        if(callback) callback({result: `Mail sent successfully to ${user.email}`});
+    } else {
+        console.log(MailConfirmationMail.html(user, link), MailConfirmationMail.text(user, link));
+        if(callback) callback({result: `Mail sent successfully to ${user.email}`});
+    }
+    
 }
 
 module.exports = sendMailConfirmationMail;
